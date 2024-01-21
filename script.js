260c599d9f3092801a7837ff8a6677bbd553b1e7
ANAND PRAKASH <122641115+AnandPrakash55@users.noreply.github.com>
2024-01-21 23:31:39 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,124 @@
+class Calculator {
+    constructor(previosOperandTextElement, currentOperandTextElement) {
+       this.previosOperandTextElement = previosOperandTextElement
+       this.currentOperandTextElement = currentOperandTextElement
+       this.clear()     
+    }
+    clear() {
+     this.currentOperand =''
+     this.previousOperand =''
+     this.operation = undefined
+    }
+
+    delete() {
+     this.currentOperand = this.currentOperand.toString().slice(0,-1)
+    }
+
+    appendNumber(number){
+        if(number === '.' && this.currentOperand.includes('.')) return
+     this.currentOperand = this.currentOperand.toString() + number.toString()
+    }
+
+    chooseOperation(operation) {
+        if(this.currentOperand === '') return
+        if(this.previousOperand !== '') {
+            this.compute()
+        }
+      this.operation = operation
+      this.previousOperand = this.currentOperand
+      this.currentOperand = ''
+    }
+
+    computer(){
+      let computation
+      const prev = parseFloat(this.previousOperand)
+      const current = parseFloat(this.currentOperand)
+      if(isNaN(prev) || isNaN(current)) return
+      switch(this.operation) {
+        case '+':
+            computation = prev + current
+            break
+            case '-':
+            computation = prev - current
+            break
+            case '*':
+            computation = prev * current
+            break
+            case '/':
+                computation = prev / current
+                break
+                default:
+                    return
+      }
+      this.currentOperand = computation
+      this.operation = undefined
+      this.previousOperand = ''
+    }
+    getDisplayNumber(number) {
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+        let integerDispaly
+        if(isNaN(integerDigits)) {
+            integerDispaly = ''
+        } else {
+            integerDispaly = integerDigits.toLocaleString('en', {maximumFractionDigits: 0})
+        }
+        if (decimalDigits != null) {
+            return $ {integerDispaly} $ {decimalDigits}
+        } else {
+            return integerDispaly
+        }
+    }
+
+    updateDisplay(){
+     this.currentOperandTextElement.innerText = this.currentOperand
+     if (this.operation != null) {
+        this.previosOperandTextElement.innerText = $this.previousOperand $ (this.operation)  
+     }
+     else {
+        this.currentOperandTextElement.innerText = '' 
+     }
+    }
+}
+
+
+
+const numberButoons = document.querySelectorAll('[data-number]')
+const  operationButtons = document.querySelectorAll('[data-number]')
+const equalButtons = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previosOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+const Calculator = new Calculator(previosOperandTextElement, currentOperandTextElement )
+
+numberButoons.forEach(button => {
+    button.addEventListener('click', () => {
+        Calculator.appendNumber(button.innerText)
+        Calculator.updateDisplay()
+    })
+})
+
+operationButoons.forEach(button => {
+    button.addEventListener('click', () => {
+        Calculator.chooseOperation(button.innerText)
+        Calculator.updateDisplay()
+    })
+})
+
+equalButton.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updateDisplay()
+}) 
+
+allClearButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete()
+    calculator.updateDisplay()
+})
